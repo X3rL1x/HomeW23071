2667987c0c606d71476915fb7adc1809a5432446
AlexKep <60612082+veliat@users.noreply.github.com>
2024-12-04 22:34:27 +0300

    xor

---
+++ Xor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string xor_encrypt(const std::string& key, const std::string& text) {
+  std::string result = "";
+  int key_len = key.length();
+  for (size_t i = 0; i < text.length(); ++i) {
+    result += (char)(text[i] ^ key[i % key_len]);
+  }
+  return result;
+}
+
+std::string xor_decrypt(const std::string& key, const std::string& encrypted_text) {
+  return xor_encrypt(key, encrypted_text);
+}
