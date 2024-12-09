39a6083e17e289388d9ecf39e80c2908db9c08fd
X3rL1x <nikitkacherkas836350@gmail.com>
2024-12-09 23:34:57 +0300

    1

--- NikitaCh/Nikita/Source1.cpp
+++ NikitaCh/Nikita/Source1.cpp
@@ -2,33 +2,21 @@
 #include <string>
 #include <chrono>
 #include <unordered_map>
+#include "Header.h"
 
 using namespace std;
 using namespace std::chrono;
 
-
-template<typename Func, typename... Args>
-auto measure_time(Func&& func, Args&&... args) {
-    auto start = high_resolution_clock::now();
-    auto result = func(forward<Args>(args)...);
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(end - start).count();
-    return make_pair(result, duration);
-}
-
-
 int main() {
-    string plaintext = "HELLO WORLD";
-    string rsa_key = "dummy_key"; 
-    string vigenere_key = "KEY";
-    string playfair_key = "PLAYFAIR";
-
-    string xor_key = "KEY";
-    auto [xor_encrypted, xor_time] = measure_time(xor_encrypt, plaintext, xor_key);
-    auto [xor_decrypted, xor_time_dec] = measure_time(xor_decrypt, xor_encrypted, xor_key);
-
-    cout << "XOR Encryption: " << xor_encrypted << " (Time: " << xor_time << " µs)" << endl;
-    cout << "XOR Decryption: " << xor_decrypted << " (Time: " << xor_time_dec << " µs)" << endl;
-
-    return 0;
+	setlocale(LC_ALL, "Russian");
+	string text = "ILOVEGUBKINUNIVERSITY";
+	string code;
+	string key = "ABCFIJSIJUHWHC";
+	cout << "Output XOR code" << endl;
+	code = encryptPlayfair(text, key);
+	cout << code << endl;
+	cout << "Output XOR decrypt" << endl;
+	code = decryptPlayfair(text, key);
+	cout << code << endl;
+	return 0;
 }
